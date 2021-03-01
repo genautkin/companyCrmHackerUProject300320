f4c37891530b5c33dbb5043ca4921480875cc49c
Gennadiy Utkin <genautkin@gmail.com>
2021-03-01 20:56:16 +0200

    Split to parts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './main-components/footer/footer.component';
+import { NavBarComponent } from './main-components/nav-bar/nav-bar.component';
+import { SideBarComponent } from './main-components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
